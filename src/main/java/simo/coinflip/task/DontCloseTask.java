7ed94bf0e0a2c538278fd0ab6bb15809de397b9f
Simo <simonixtgoi@gmail.com>
2026-08-08 13:46:29 +0200

    Update ChoosingGUI every 20Ticks DontCloseTask running every tick CreatingQueue changed from list to map Methods removeValue and addValue from coinflip are now with parameter for amount ClickEvent cancelled when on all the screens of the coinflipping and result Now player cannot join a coinflip if he is on queue

---
+++ src/main/java/simo/coinflip/task/DontCloseTask.java
@@ -0,0 +1,30 @@
+package simo.coinflip.task;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class DontCloseTask extends BukkitRunnable {
+    private final Inventory inventory;
+    private final Player player;
+    private final int duration;
+    private int count = 0;
+
+    public DontCloseTask(Player player, Inventory inventory, int duration_in_seconds) {
+        this.inventory = inventory;
+        this.player = player;
+        this.duration = duration_in_seconds*20;
+    }
+
+    @Override
+    public void run() {
+        if(!player.getOpenInventory().equals(inventory)) {
+            player.openInventory(inventory);
+        }
+        if(count >= duration) {
+            cancel();
+            return;
+        }
+        count++;
+    }
+}
---
+++ src/main/java/simo/coinflip/task/UpdateChoosingGUI.java
@@ -0,0 +1,26 @@
+package simo.coinflip.task;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.scheduler.BukkitRunnable;
+import simo.coinflip.gui.ChoosingGUI;
+
+public class UpdateChoosingGUI extends  BukkitRunnable {
+    private final Player player;
+    private final ChoosingGUI choosingGUI;
+    private final Inventory inv;
+
+    public UpdateChoosingGUI(Player player, ChoosingGUI choosingGUI, Inventory inv) {
+        this.player = player;
+        this.choosingGUI = choosingGUI;
+        this.inv = inv;
+    }
+
+    @Override
+    public void run() {
+        if(!player.getOpenInventory().getTopInventory().equals(inv)) {
+            cancel();
+        }
+        choosingGUI.updateChoosingGUI(inv);
+    }
+}
